7322d8c7bbb241b67020620c9ddfd344ca3c2ac2
Macaroni Biff <corey.waitforit.mitchell@gmail.com>
2018-07-07 19:50:41 -0500

    I think i finally fixed the issue. I also realised that I never put the game in a document.ready function... maybe that's what fixed it somehow.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,73 +1,60 @@
-//Creating initial variables
-var playerTotal = 0; 
-var wins = 0;
-var losses = 0;
+$(document).ready(function() {
+  console.log("ready")
 
-//Creates random number to guess
-var randomNumber = Math.floor(Math.random()*102+19)
-  
-//Displays variable
-$("#wins").text(wins);
-$("#losses").text(losses);
-$("#scoreToMatch").text(randomNumber);
-  
-//Creates random number for each crystal
-var redCrystal = Math.floor(Math.random()*12+1)
-var blueCrystal = Math.floor(Math.random()*12+1)
-var yellowCrystal = Math.floor(Math.random()*12+1)
-var greenCrystal = Math.floor(Math.random()*12+1)
+  //Creating initial variables
+  var playerTotal = 0; 
+  var wins = 0;
+  var losses = 0;
 
-//Reset game function
-function reset(){
-  randomNumber = Math.floor(Math.random()*102+19);
-  $("#scoreToMatch").text(randomNumber);
-  redCrystal = Math.floor(Math.random()*12+1);
-  blueCrystal = Math.floor(Math.random()*12+1);
-  yellowCrystal = Math.floor(Math.random()*12+1);
-  greenCrystal = Math.floor(Math.random()*12+1);
-  playerTotal = 0;
-  $("#userScore").text(playerTotal);
-} 
-
-//Win scenerio function
-function win(){
-  $("#scenerio").text("You Won!");
-  wins++; 
+  //Creates random number to guess
+  var randomNumber = Math.floor(Math.random()*102+19)
+    
+  //Displays variable
   $("#wins").text(wins);
-  reset();
-}
-
-//Loss scenerio function
-function lose(){
-  $("#scenerio").text("You Lose :(");
-  losses++;
   $("#losses").text(losses);
-  reset()
-}
+  $("#scoreToMatch").text(randomNumber);
+    
+  //Creates random number for each crystal
+  var redCrystal = Math.floor(Math.random()*12+1)
+  var blueCrystal = Math.floor(Math.random()*12+1)
+  var yellowCrystal = Math.floor(Math.random()*12+1)
+  var greenCrystal = Math.floor(Math.random()*12+1)
 
-//Clicking crystals
+  //Reset game function
+  function reset(){
+    randomNumber = Math.floor(Math.random()*102+19);
+    $("#scoreToMatch").text(randomNumber);
+    redCrystal = Math.floor(Math.random()*12+1);
+    blueCrystal = Math.floor(Math.random()*12+1);
+    yellowCrystal = Math.floor(Math.random()*12+1);
+    greenCrystal = Math.floor(Math.random()*12+1);
+    playerTotal = 0;
+    $("#userScore").text(playerTotal);
+  } 
 
-//Red Crystal
-$("#red-crystal").on("click", function(){
-  playerTotal = playerTotal + redCrystal;
-
-  $("#userScore").text(playerTotal); 
-
-  if (playerTotal === randomNumber){
-    win();
+  //Win scenerio function
+  function win(){
+    $("#scenerio").text("You Won!");
+    wins++; 
+    $("#wins").text(wins);
+    reset();
   }
 
-  else if (playerTotal > randomNumber){
-    lose();
-  }   
+  //Loss scenerio function
+  function lose(){
+    $("#scenerio").text("You Lose :(");
+    losses++;
+    $("#losses").text(losses);
+    reset()
+  }
 
-})  
+  //Clicking crystals
 
-//Blue Crystal
-$("#blue-crystal").on("click", function(){
-  playerTotal = playerTotal + blueCrystal;
+  //Red Crystal
+  $("#red-crystal").on("click", function(){
+    playerTotal = playerTotal + redCrystal;
 
-  $("#userScore").text(playerTotal); 
+    $("#userScore").text(playerTotal); 
 
     if (playerTotal === randomNumber){
       win();
@@ -75,35 +62,53 @@ $("#blue-crystal").on("click", function(){
 
     else if (playerTotal > randomNumber){
       lose();
+    }   
+
+  })  
+
+  //Blue Crystal
+  $("#blue-crystal").on("click", function(){
+    playerTotal = playerTotal + blueCrystal;
+
+    $("#userScore").text(playerTotal); 
+
+      if (playerTotal === randomNumber){
+        win();
+      }
+
+      else if (playerTotal > randomNumber){
+        lose();
+      } 
+  })  
+
+  //Yellow Crystal
+  $("#yellow-crystal").on("click", function(){
+    playerTotal = playerTotal + yellowCrystal;
+
+    $("#userScore").text(playerTotal);
+
+    if (playerTotal === randomNumber){
+        win();
+    }
+
+    else if (playerTotal > randomNumber){
+      lose();
     } 
-})  
+  }) 
+    
+  //Green Crystal
+  $("#green-crystal").on("click", function(){
+    playerTotal = playerTotal + greenCrystal;
 
-//Yellow Crystal
-$("#yellow-crystal").on("click", function(){
-  playerTotal = playerTotal + yellowCrystal;
-
-  $("#userScore").text(playerTotal);
-
-  if (playerTotal === randomNumber){
+    $("#userScore").text(playerTotal); 
+        
+    if (playerTotal === randomNumber){
       win();
-  }
+    }
 
-  else if (playerTotal > randomNumber){
-    lose();
-  } 
-}) 
-  
-//Green Crystal
-$("#green-crystal").on("click", function(){
-  playerTotal = playerTotal + greenCrystal;
+    else if (playerTotal > randomNumber){
+      lose();
+    }
+  });   
 
-  $("#userScore").text(playerTotal); 
-      
-  if (playerTotal === randomNumber){
-    win();
-  }
-
-  else if (playerTotal > randomNumber){
-    lose();
-  }
-});   
+})
